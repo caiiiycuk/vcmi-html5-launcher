40db0a9147c3f40dc601d633e62cf055021fb86b
Alexey <alex.v.timofeev@gmail.com>
2024-12-18 20:06:27 +0500

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,7 +7,7 @@ import { State, version } from "./util/store";
 const translations: {[lang: string]: {[key: string]: string} } = {
     ru: {
         title: "Герои Меча и Магии III" + " " + version,
-        confirm_legal_copy: "У меня есть легальная копия игры",
+        confirm_legal_copy: "Я подтверждаю, что у меня есть легальная копия игры.",
         legal: "Права",
         legal_text: "Игра основана на движке с открытым исходныи кодом VCMI[1]. Что бы начать играть необходимо указать URL с данными оригинальной игры HoMM3[2], либо вы можете загрузить их. Вы не можете продолжить если Вы не приобретали оригинальную игру.",
         data_source: "Источник данных",
@@ -18,7 +18,7 @@ const translations: {[lang: string]: {[key: string]: string} } = {
         loading_data_title: "Загрузка данных...",
         error: "Ошибка",
         open_browser_logs: "Откройте лог браузера для подробной информации...",
-        start_the_game: "Запустить",
+        start_the_game: "Запустить игру",
         loading_db: "Загрузка локальных данных...",
         config: "Конфигурация",
         resolution: "Разрешение",
@@ -42,9 +42,9 @@ const translations: {[lang: string]: {[key: string]: string} } = {
     },
     en: {
         title: "Heroes Of Might And Magic III" + " " + version,
-        confirm_legal_copy: "I confirm I have legal copy of this game.",
+        confirm_legal_copy: "I confirm that I have a legal copy of the game.",
         legal: "Legal",
-        legal_text: "The game based on opensource engine VCMI[1]. To play the game you need to provide URL to original HoMM3[2] game files, or you can upload them. You can't continue if you do not own the game",
+        legal_text: "The game based on opensource engine VCMI[1]. To start playing, you need to provide the URL with data of the original HoMM3[2] game files, or you can upload them. You can't continue if you have not purchased the original game.",
         data_source: "Data source",
         data_directory: "Installed game (select folder)",
         data_db: "Already uploaded",
